62ea4a808397c87c379bc8584ad16b83d3f1948c
mitro54 <152814815+mitro54@users.noreply.github.com>
2024-02-22 18:57:00 +0200

    Added more logic
    Added working logic to the game, added comments to make it clearer.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Random computer output
 function getComputedChoice() {
     let randomNumber = Math.floor(Math.random() * 99) + 1
     if (randomNumber <= 33) {
@@ -10,15 +11,35 @@ function getComputedChoice() {
 };
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection === 'rock') {
-        return 'Computer selected rock, draw!'
-    } else if (computerSelection === 'paper') {
-        return 'Computer selected paper, computer wins!'
-    } else {
-        return 'Computer selected scissors, you win!';
+    // Draws
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'Both players selected rock, draw!'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'Both players selected paper, draw!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'Both players selected scissors, draw!'
+    }
+    // Player inputs paper
+    if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'Paper beats rock, player wins!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'Scissors beats paper, computer wins!'
+    }
+    // Player inputs scissors
+    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'Scissors beats paper, player wins!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'Rock beats scissors, computer wins!'
+    }
+    // Player inputs rock
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'Paper beats rock, computer wins!'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'Rock beats scissors, player wins!'
     }
 }
 
-const playerSelection = 'rock';
+const playerSelection = prompt();
+const playerSelectionFixer = playerSelection.toLowerCase()
 const computerSelection = getComputedChoice();
-console.log(playRound(playerSelection, computerSelection))
+console.log(playRound(playerSelectionFixer, computerSelection))
