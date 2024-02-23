d1ac5280b3895e30b34848cf2b9974eac0f56be6
mitro54 <152814815+mitro54@users.noreply.github.com>
2024-02-23 14:28:08 +0200

    Fixed it, for the time being, works properly Had an issue with the code, had to create inputValidator, fix syntax.

--- script.js
+++ script.js
@@ -2,18 +2,7 @@ let userWinCount = 0;
 let computerWinCount = 0;
 let drawIdentify = false;
 let playerPrompt = prompt();
-const playerSelection = playerPrompt.toLowerCase();
-
-
-// Validating user input
-function inputValidator(playerPrompt) {
-    if (playerPrompt === 'rock' || 'paper' || 'scissors') {
-        return playerSelection;
-    } else {
-        console.log('Cannot identify user input...');
-        return playerPrompt = prompt();
-    }
-};
+let playerSelection = playerPrompt.toLowerCase();
 
 // Random computer output
 function getComputedChoice() {
@@ -27,32 +16,32 @@ function getComputedChoice() {
     }
 };
 
-function playRound(playerInput, getComputedChoice) {
+function playRound(playerSelection, getComputedChoice) {
     // Draws
-    if (playerInput === getComputedChoice) {
+    if (playerSelection === getComputedChoice) {
         console.log(`Both players selected ${playerSelection}, draw!`);
     }
     // Player inputs paper
-    if (playerInput === 'paper' && getComputedChoice === 'rock') {
+    if (playerSelection === 'paper' && getComputedChoice === 'rock') {
         userWinCount++;
         console.log('Paper beats rock, player wins!');
-    } else if (playerInput === 'paper' && getComputedChoice === 'scissors') {
+    } else if (playerSelection === 'paper' && getComputedChoice === 'scissors') {
         computerWinCount++;
         console.log('Scissors beats paper, computer wins!');
     }
     // Player inputs scissors
-    if (playerInput === 'scissors' && getComputedChoice === 'paper') {
+    if (playerSelection === 'scissors' && getComputedChoice === 'paper') {
         userWinCount++;
         console.log('Scissors beats paper, player wins!');
-    } else if (playerInput === 'scissors' && getComputedChoice === 'rock') {
+    } else if (playerSelection === 'scissors' && getComputedChoice === 'rock') {
         computerWinCount++;
         console.log('Rock beats scissors, computer wins!');
     }
     // Player inputs rock
-    if (playerInput === 'rock' && getComputedChoice === 'paper') {
+    if (playerSelection === 'rock' && getComputedChoice === 'paper') {
         computerWinCount++;
         console.log('Paper beats rock, computer wins!');
-    } else if (playerInput === 'rock' && getComputedChoice === 'scissors') {
+    } else if (playerSelection === 'rock' && getComputedChoice === 'scissors') {
         userWinCount++;
         console.log('Rock beats scissors, player wins!');
     }
@@ -60,7 +49,7 @@ function playRound(playerInput, getComputedChoice) {
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
-        playRound(inputValidator(), getComputedChoice())
+        playRound(playerSelection, getComputedChoice())
     }
 };
 
@@ -78,12 +67,31 @@ function winTest() {
             console.log('Retrying...');
             playGame();
             drawIdentify = false;
+            winTest();
         }
-        console.log(`After the long and tedious battle the final score is: Player with ${userWinCount} and Computer with ${computerWinCount} ...`);
-        userWinCount > computerWinCount ? console.log('Player wins!') : console.log('Computer wins!');
+        //  console.log(`After the long and tedious battle the final score is: Player with ${userWinCount} and Computer with ${computerWinCount} ...`);
+        //   userWinCount > computerWinCount ? console.log('Player wins!') : console.log('Computer wins!');
     }
-}
+};
+function inputValidator(playerSelection) {
+    switch (playerSelection) {
+        case 'rock':
+            return true;
+        case 'paper':
+            return true;
+        case 'scissors':
+            return true;
+        default:
+            return false;
+    }
+};
 
-playGame();
-winTest();
+if (inputValidator(playerSelection) === true) {
+    setTimeout(() => {
+        playGame();
+        winTest();
+    }, 2000);
+} else {
+    console.log('Please input rock, paper or scissors, refresh the page!');
+}
 
